c5ca6afea094e57df4bf6b9dd6530582e1edbf49
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-03-24 12:06:52 +0600

    add global version variable

--- main.go
+++ main.go
@@ -2,21 +2,26 @@ package main
 
 import "github.com/docopt/docopt-go"
 
-const usage = `bmo - missing tool for parsing structured text.
+var (
+	version = "1.0"
+	usage   = `bmo - missing tool for parsing structured text.
 
 Usage:
-    bmo -h | --help
     bmo (-b | --blocks) <awk_range_begin> <awk_range_end> <awk_condition>
+    bmo -h | --help
+    bmo --version
 
 Options:
-    -h --help    Show this help.
     -b --blocks  Match blocks using <awk_range_begin>, <awk_range_end> and
                  output it only if <awk_condition> is true for at least on line
                  in the block.
+    -h --help    Show this help.
+    --version    Show version.
 `
+)
 
 func main() {
-	args, err := docopt.Parse(usage, nil, true, "1.0", false)
+	args, err := docopt.Parse(usage, nil, true, version, false)
 	if err != nil {
 		panic(err)
 	}
