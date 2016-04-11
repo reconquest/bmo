82945ac1c32d89c19ce39d3089a59d04f3110e23
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-04-11 19:03:20 +0600

    /testcases/ same begin and end conditions

---
+++ tests/testcases/blocks/extract-blocks-with-same-begin-and-end.test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+tests:put input <<EOF
+time 1
+aaa  1
+time 2
+aaa  2
+time 3
+bbb  3
+EOF
+
+tests:eval cat input \| bmo -b '/time/' '/time/' '/aaa/'
+tests:assert-success
+
+tests:assert-no-diff stdout <<EOF
+time 1
+aaa  1
+time 2
+time 2
+aaa  2
+time 3
+EOF
