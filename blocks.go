574e782f6d6a019d52692a3b2d74b0c25726c75e
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-04-11 17:53:20 +0600

    fix wrong block handling order

--- blocks.go
+++ blocks.go
@@ -10,6 +10,31 @@ import (
 )
 
 var parseBlocksAWKProgram = template.Must(template.New("blocks").Parse(`
+	function handle_block_inner() {
+		if (block_contents) {
+			block_contents = block_contents "\n" $0
+		} else {
+			block_contents = $0
+		}
+	}
+
+	function handle_block_end() {
+		current_line = $0
+
+		$0 = block_contents
+		if ({{ $.Condition }}) {
+			print block_contents
+		}
+
+		$0 = current_line
+	}
+
+	in_range && ({{ $.RangeEnd }}) {
+		handle_block_inner()
+		handle_block_end();
+		in_range = 0;
+	}
+
 	{{ $.RangeBegin }} {
 		in_range = 1;
 		use_block = 0;
@@ -17,24 +42,7 @@ var parseBlocksAWKProgram = template.Must(template.New("blocks").Parse(`
 	}
 
 	in_range {
-		if (block_contents) {
-			block_contents = block_contents "\n" $0
-		} else {
-			block_contents = $0
-		}
-
-		$0 = block_contents
-		if (!use_block && ({{ $.Condition }})) {
-			use_block = 1;
-		}
-	}
-
-	{{ $.RangeEnd }} {
-		if (use_block) {
-			print block_contents
-		}
-
-		in_range = 0;
+		handle_block_inner()
 	}
 `))
 
